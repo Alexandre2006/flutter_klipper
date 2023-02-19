0a0bffa855e039fd0e343467876cca4536dbf1e7
Alexandre Haddad-Delaveau <ahaddaddelaveau@gmail.com>
2023-02-19 12:04:14 -0800

    Initial Commit

---
+++ example/flutter_klipper_example.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_klipper/flutter_klipper.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/flutter_klipper.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library flutter_klipper;
+
+export 'src/flutter_klipper_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
