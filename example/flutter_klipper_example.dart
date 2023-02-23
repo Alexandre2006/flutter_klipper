4cc228f5ffbdc2438b63e2b718e8b9b089c59df5
Alexandre Haddad-Delaveau <ahaddaddelaveau@gmail.com>
2023-02-23 15:23:55 -0800

    Added Support for Download/Uploading Files

--- example/flutter_klipper_example.dart
+++ example/flutter_klipper_example.dart
@@ -1,24 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter_klipper/flutter_klipper.dart';
 
-void main() {
+void main() async {
   // Example Coming Soon!
-  Klipper('raspberrypi.local').statusNotifier
-    ..disconnected.addListener((state) {
-      print("Client Disconnected");
-    }, fireImmediately: false,)
-    ..ready.addListener((state) {
-      print("Client Ready");
-    }, fireImmediately: false,)
-    ..error.addListener((state) {
-      print("Client Error");
-    }, fireImmediately: false,)
-    ..initializing.addListener((state) {
-      print("Client Initializing");
-    }, fireImmediately: false,)
-    ..shutdown.addListener((state) {
-      print("Client Shutdown");
-    }, fireImmediately: false,)
-    ..startup.addListener((state) {
-      print("Client Starting");
-    }, fireImmediately: false,);
+  Klipper('raspberrypi.local');
 }
